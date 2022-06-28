9cf623678cb44fe86358978b8ba4de0f611b0437
screenshot-zz <qq252172547@gmail.com>
2022-06-28 10:25:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/immortalwrt-collections/openwrt-gowebdav.git package/gowebdav
